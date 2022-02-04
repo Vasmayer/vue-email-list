a2acd5de3706136a14582df42f68ae839b3de0f9
Vasmayer <lealimarco90@gmail.com>
2022-02-04 15:08:20 +0100

    basics structure to start

---
+++ js/script.js
@@ -0,0 +1,20 @@
+/* 
+
+Descrizione:
+Attraverso l'apposita API di Boolean
+https://flynn.boolean.careers/exercises/api/random/mail
+Usando Vue.js, generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
+*/
+
+Vue.config.devtools = true;
+
+var root = new Vue({
+    el: '#root',
+    data:{
+    },
+    methods:
+    {
+      
+    } 
+  });
